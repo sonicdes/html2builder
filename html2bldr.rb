b9baad9942623c844ec201314937a6dc78cfccf9
Denis Sadomowski <sonicdes@gmail.com>
2020-02-11 18:04:21 +0300

    handle comments and escape single quotes in strings

--- html2bldr.rb
+++ html2bldr.rb
@@ -1,18 +1,21 @@
 require 'oga'
 
 def parse_block(element, indent=0)
-  if element.is_a? Oga::XML::Text
+  case element
+  when Oga::XML::Text
     return '' if element.text.strip.empty?
-    return " "*indent + "'#{element.text.strip}' \n"
+    return " "*indent + "'#{element.text.strip.gsub("'"){"\\'"}}' \n"
+  when Oga::XML::Document
+    return element.children.map {|ch| parse_block ch}
+  when Oga::XML::Comment
+    return " "*indent + "\# #{element.text.strip}\n"
+  when Oga::XML::Element
+    attrs = element.attributes.map {|attr| "'#{attr.name}' => '#{attr.value}'"}.join(", ")
+    pre = " "*indent + [element.name, attrs].join(" ") + " do\n"
+    post = " "*indent + "end\n"
+    inner_block = element.children.map {|ch| parse_block(ch, indent+2)}
+    return [pre, inner_block, post].join
   end
-
-  return element.children.map {|ch| parse_block ch} if element.is_a? Oga::XML::Document
-
-  attrs = element.attributes.map {|attr| "'#{attr.name}' => '#{attr.value}'"}.join(", ")
-  pre = " "*indent + [element.name, attrs].join(" ") + " do\n"
-  post = " "*indent + "end\n"
-  inner_block = element.children.map {|ch| parse_block(ch, indent+2)}
-  return [pre, inner_block, post].join
 end
 
 doc = Oga.parse_html(File.read(ARGV[0]))
